19b3842506d77ce93ac500c84f41dc81b50e5197
duchuyvo0368 <voduchuy016688@gmail.com>
2024-11-30 21:47:00 +0700

    database1

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,6 @@ mongoose
 app.get("/ping", (_req, res) => {
   return res.json({ msg: "Ping Successful" });
 });
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
